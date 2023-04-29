5893de22d599dc4715a73c4686605a9b8a2b017e
Esipov Aleksey <a_esipov@list.ru>
2023-04-29 16:52:07 +0300

    Добавил плагин для JUnit5 - org.apache.maven.plugins:maven-surefire-plugin:3.0.0-M7

--- order/src/test/java/ru/job4j/OrderMainAppTest.java
+++ order/src/test/java/ru/job4j/OrderMainAppTest.java
@@ -1,9 +1,7 @@
 package ru.job4j;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-@Disabled
 @SpringBootTest
 class OrderMainAppTest {
     @Test
