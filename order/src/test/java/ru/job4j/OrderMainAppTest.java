06fb4c977347fbfff2ae221796117116ba88bab1
Esipov Aleksey <a_esipov@list.ru>
2023-04-29 16:41:50 +0300

    Отключил OrderMainAppTest

--- order/src/test/java/ru/job4j/OrderMainAppTest.java
+++ order/src/test/java/ru/job4j/OrderMainAppTest.java
@@ -1,7 +1,9 @@
 package ru.job4j;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+@Disabled
 @SpringBootTest
 class OrderMainAppTest {
     @Test
